d1778db46d975020cf4b4e43af4050c2b0d26227
TheHelix09 <87874916+TheHelix09@users.noreply.github.com>
2023-10-14 14:53:15 -0600

    START

---
+++ vista/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CEREC</title>
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Navbar -->
+      <?php  include 'includes/navbar.php'?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <?php  include 'includes/menu.php'?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+ 
+
+    <!-- Main content -->
+    <section class="content">
+      
+    </section>
+    <!-- /.content -->
+  </div>
+
+  <?php  include 'includes/footer.php'?>
+
+</div>
+
+</body>
+</html>
